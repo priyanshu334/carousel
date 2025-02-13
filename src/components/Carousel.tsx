ef999135fa29cb24b6007d1cc4670b5eb7047451
priyanshu shrivastava <99priyanshu33@gmail.com>
2025-02-13 11:26:35 +0530

    carousel

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./Carousel.css";
+
+const images = [
+  "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?cs=srgb&dl=pexels-soldiervip-1308881.jpg&fm=jpg",
+  "https://thumbs.dreamstime.com/b/young-conceptual-image-large-stone-shape-human-brain-conceptual-image-large-stone-shape-110748113.jpg",
+  "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?cs=srgb&dl=pexels-soldiervip-1308881.jpg&fm=jpg",
+];
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="carousel">
+      <div className="carousel-inner">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Slide ${index}`}
+            className={`carousel-image ${index === currentIndex ? "active" : ""}`}
+          />
+        ))}
+      </div>
+      <button className="prev" onClick={prevSlide}>&#10094;</button>
+      <button className="next" onClick={nextSlide}>&#10095;</button>
+    </div>
+  );
+};
+
+export default Carousel;
